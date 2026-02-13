66795a7b6a033e2f486ed5dbca3bedb945fa52b9
Alleluia <alleluiacervi@gmail.com>
2026-02-13 23:25:16 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    esmExternals: "loose",
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.module.strictExportPresence = false;
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig
